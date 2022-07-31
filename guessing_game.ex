ccb05137ac856c13c0f1c9e5196fa4d8415fed4e
Victor <vitox320@gmail.com>
2022-07-31 10:39:43 -0300

    guessing_game exercism

---
+++ guessing_game.ex
@@ -0,0 +1,15 @@
+defmodule GuessingGame do
+
+  def compare(secret_number, guess \\ nil)
+
+  def compare(_secret_number, guess) when is_nil(guess) or not is_number(guess) , do: "Make a guess"
+
+  def compare(secret_number, guess) when secret_number === guess, do: "Correct"
+
+  def compare(secret_number, guess) when guess == secret_number - 1 or guess == secret_number + 1, do: "So close"
+
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
+
+end
